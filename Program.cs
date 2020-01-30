1b4fa6a0ce0ce3001427dcbb36ec5164e2637de6
Bryce Roberts <kendall13@usf.edu>
2020-01-30 18:02:13 -0500

    Iterative Statements

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Iterative_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display what the user is supposed to insert
+            Console.WriteLine("Enter an interger between 1 and 100");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            try
+            {
+                //Checks if the number is greater then 100 or less the 0
+                if ((number >= 0) && (number <= 100))
+                {
+                    for (int count = 1; count <= number; count++)
+                    {
+                        Console.WriteLine("You have entered " + number.ToString() + ". "+"This is the current integer for the value in the loop: "+ count.ToString());
+                    }
+                }
+                else
+                {
+                    //Error message telling the user they have entered a value too high or too low 
+                    Console.WriteLine("You have entered an ivalid data type");
+                }
+            }
+            catch
+            {
+                    Console.WriteLine("You have entered an ivalid data type");
+            }
+        }
+        
+    }
+}
